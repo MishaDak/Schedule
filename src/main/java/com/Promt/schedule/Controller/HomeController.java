180c1c4b233635fbb89695faec901c5fc1b841a8
MishaDak <dack.misha@gmail.com>
2021-01-28 00:41:53 +0700

    Initial project

---
+++ src/main/java/com/Promt/schedule/Controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.Promt.schedule.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/index")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("tile", name);
+        return "index";
+    }
+
+
+
+}
