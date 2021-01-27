2c4c43b316d03b60f07028c696b67dc7212a2d14
MishaDak <dack.misha@gmail.com>
2021-01-28 04:21:09 +0700

    Add footer and navbar

--- src/main/java/com/Promt/schedule/Controller/HomeController.java
+++ src/main/java/com/Promt/schedule/Controller/HomeController.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
     @GetMapping("/index")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("tile", name);
+    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("title", name);
         return "index";
     }
 
-
+    @GetMapping("/about")
+    public String schedule(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("title", name);
+        return "about";
+    }
 
 }
